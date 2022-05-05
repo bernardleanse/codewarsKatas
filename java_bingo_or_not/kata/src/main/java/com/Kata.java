b7453ff04fac3a859a0b2c4b0083a4940763404a
Bernard Leanse <bernardleanse96@gmail.com>
2022-05-05 10:22:40 +0100

    extract letter list getter method

--- java_bingo_or_not/kata/src/main/java/com/Kata.java
+++ java_bingo_or_not/kata/src/main/java/com/Kata.java
@@ -1,16 +1,26 @@
 package com;
+import java.util.ArrayList;
 
 public class Kata 
 {
-    private static String convertNumToAlpha(int num)
+    private static String convertNumToLetter(int num)
     {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         return alphabet.substring(num - 1, num);
     }
+    private static ArrayList<String> getLetterList(int[] nums)
+    {
+        ArrayList<String> letters = new ArrayList<String>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            letters.add(convertNumToLetter(nums[i]));
+        }
+        return letters;
+    }
     private static boolean includesBingo(int[] numberArray)
     {   
-        convertNumToAlpha(numberArray[0]);
-        return false;
+        ArrayList<String> letters = getLetterList(numberArray);
+        return true;
     }
     public static String bingo(int[] numberArray)
     {
